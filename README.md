55656d5cff26be95e6075d3861e863b6b8e36a31
François Massiot <francois.massiot@outlook.fr>
2017-10-17 10:56:54 +0200

    Depot1-mise-a-jour-fichiers

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ReminderDerniereVersion
+
+Modifier uniquement le fichier "V0.3-Assemblage"
+Ne pas toucher aux dossiers "V0.1-Base" et "V0.2-Fonctionnalités"
---
+++ V0.3-Assemblage/Reminder V0.2/Connexion/App.js
@@ -0,0 +1,40 @@
+
+
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login/Login';
+
+export default class GithubApp extends Component {
+   render() {
+    return (
+      <Login />
+    );
+   }
+  }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       backgroundColor: '#F5FCFF',
+//     },
+
+//     welcome: {
+//       fontSize: 20,
+//       textAlign: 'center',
+//       margin: 10,
+//    }
+// });
+
+
+
+
+
+
+
+
+
+
+
+  
